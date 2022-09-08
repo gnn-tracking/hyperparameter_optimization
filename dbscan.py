8c2d95cc5745f8769027b2b804440a573931ed96
Kilian Lieret <kilian.lieret@posteo.de>
2022-09-08 16:33:32 -0400

    Add dbscan with cudf example

---
+++ dbscan.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import cudf
+from cuml.cluster import DBSCAN
+
+# Create and populate a GPU DataFrame
+gdf_float = cudf.DataFrame()
+gdf_float['0'] = [1.0, 2.0, 5.0]
+gdf_float['1'] = [4.0, 2.0, 1.0]
+gdf_float['2'] = [4.0, 2.0, 1.0]
+
+# Setup and fit clusters
+dbscan_float = DBSCAN(eps=1.0, min_samples=1)
+dbscan_float.fit(gdf_float)
+
+print(dbscan_float.labels_)
