5358f1b8ddb77f19fbe4433b94f0a602a423fb55
Kilian Lieret <kilian.lieret@posteo.de>
2022-10-24 13:30:26 -0400

    Split up re-useable and exp-specific part of tune.py

--- scripts/tune.py
+++ scripts/tune.py
@@ -4,10 +4,8 @@ from __future__ import annotations
 
 from functools import partial
 from pathlib import Path
-from typing import Any
 
 import click
-import optuna
 from ray import tune
 from ray.air import CheckpointConfig, FailureConfig, RunConfig
 from ray.air.callbacks.wandb import WandbLoggerCallback
@@ -15,46 +13,14 @@ from ray.tune import SyncConfig
 from ray.tune.schedulers import ASHAScheduler
 from ray.tune.search.optuna import OptunaSearch
 from util import (
-    TCNTrainable,
     della,
-    get_fixed_config,
     get_points_to_evaluate,
     maybe_run_distributed,
     read_json,
     run_wandb_offline,
-    suggest_if_not_fixed,
 )
 
 server = della
-
-
-def suggest_config(
-    trial: optuna.Trial, *, test=False, fixed: dict[str, Any] | None = None
-) -> dict[str, Any]:
-    # Everything with prefix "m_" is passed to the model
-    # Everything with prefix "lw_" is treated as loss weight kwarg
-    fixed_config = get_fixed_config(test=test)
-    if fixed is not None:
-        fixed_config.update(fixed)
-
-    def sinf_float(key, *args, **kwargs):
-        suggest_if_not_fixed(trial.suggest_float, key, fixed_config, *args, **kwargs)
-
-    def sinf_int(key, *args, **kwargs):
-        suggest_if_not_fixed(trial.suggest_int, key, fixed_config, *args, **kwargs)
-
-    sinf_float("q_min", 1e-3, 1, log=True)
-    sinf_float("sb", 0, 1)
-    sinf_float("lr", 2e-6, 1e-3, log=True)
-    sinf_int("m_hidden_dim", 64, 256)
-    sinf_int("m_L_ec", 1, 7)
-    sinf_int("m_L_hc", 1, 7)
-    sinf_float("focal_gamma", 0, 20)  # 5 might be a good default
-    sinf_float("focal_alpha", 0, 1)  # 0.95 might be a good default
-    sinf_float("lw_edge", 0.001, 500)
-    sinf_float("lw_potential_attractive", 1, 500)
-    sinf_float("lw_potential_repulsive", 1e-2, 1e2)
-    return fixed_config
 
 
 @click.command()
@@ -86,6 +52,8 @@ def suggest_config(
     help="Fix config values to these values",
 )
 def main(
+    trainable,
+    suggest_config,
     *,
     test=False,
     gpu=False,
@@ -117,7 +85,7 @@ def main(
 
     tuner = tune.Tuner(
         tune.with_resources(
-            TCNTrainable,
+            trainable,
             {"gpu": 1 if gpu else 0, "cpu": server.cpus_per_gpu if not test else 1},
         ),
         tune_config=tune.TuneConfig(
@@ -147,7 +115,3 @@ def main(
         ),
     )
     tuner.fit()
-
-
-if __name__ == "__main__":
-    main()
---
+++ scripts/tune_01.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+from typing import Any
+
+import optuna
+from tune import main
+from util import TCNTrainable, get_fixed_config, suggest_if_not_fixed
+
+
+def suggest_config(
+    trial: optuna.Trial, *, test=False, fixed: dict[str, Any] | None = None
+) -> dict[str, Any]:
+    # Everything with prefix "m_" is passed to the model
+    # Everything with prefix "lw_" is treated as loss weight kwarg
+    fixed_config = get_fixed_config(test=test)
+    if fixed is not None:
+        fixed_config.update(fixed)
+
+    def sinf_float(key, *args, **kwargs):
+        suggest_if_not_fixed(trial.suggest_float, key, fixed_config, *args, **kwargs)
+
+    def sinf_int(key, *args, **kwargs):
+        suggest_if_not_fixed(trial.suggest_int, key, fixed_config, *args, **kwargs)
+
+    sinf_float("q_min", 1e-3, 1, log=True)
+    sinf_float("sb", 0, 1)
+    sinf_float("lr", 2e-6, 1e-3, log=True)
+    sinf_int("m_hidden_dim", 64, 256)
+    sinf_int("m_L_ec", 1, 7)
+    sinf_int("m_L_hc", 1, 7)
+    sinf_float("focal_gamma", 0, 20)  # 5 might be a good default
+    sinf_float("focal_alpha", 0, 1)  # 0.95 might be a good default
+    sinf_float("lw_edge", 0.001, 500)
+    sinf_float("lw_potential_attractive", 1, 500)
+    sinf_float("lw_potential_repulsive", 1e-2, 1e2)
+    return fixed_config
+
+
+if __name__ == "__main__":
+    main(TCNTrainable, suggest_config)
