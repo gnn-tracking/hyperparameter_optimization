176469d0457edaa65a8a119130380d06ac99d188
Kilian Lieret <kilian.lieret@posteo.de>
2022-11-14 18:30:38 -0500

    Cruft update (#8)

---
+++ src/gte/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+try:
+    from importlib.metadata import version
+except ModuleNotFoundError:
+    from importlib_metadata import version  # type: ignore
+
+
+__version__ = version("gnn_tracking_experiments")
