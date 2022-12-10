1e24a613388c31aab3a03fdc8f4976073ba5bcfd
Kilian Lieret <kilian.lieret@posteo.de>
2022-12-10 14:59:42 -0500

    Refactor ThresholdByEpochStopper

--- src/gnn_tracking_hpo/util/stoppers.py
+++ src/gnn_tracking_hpo/util/stoppers.py
@@ -1,12 +1,13 @@
 from __future__ import annotations
 
 import collections
+from math import isnan
 from typing import Any, DefaultDict
 
 from ray import tune
 
 
-class ResultThresholdStopper(tune.Stopper):
+class ThresholdByEpochStopper(tune.Stopper):
     def __init__(self, metric: str, thresholds: dict[int, float], mode: str = "max"):
         """Stopper that stops if results at a certain epoch fall above/below a certain
         threshold.
@@ -16,18 +17,26 @@ class ResultThresholdStopper(tune.Stopper):
         self.mode = mode
 
     def get_threshold(self, epoch: int) -> float:
-        last_specified_epoch = max([k for k in self.threshold.keys() if k <= epoch])
-        return self.threshold[last_specified_epoch]
+        relevant_epoch = max([k for k in self.threshold if k <= epoch], default=-1)
+        if relevant_epoch < 0:
+            return float("nan")
+        assert relevant_epoch in self.threshold
+        return self.threshold[relevant_epoch]
+
+    def _better_than(self, a: float, b: float) -> bool:
+        if self.mode == "max":
+            return a > b
+        elif self.mode == "min":
+            return a < b
+        else:
+            raise ValueError(f"Invalid mode {self.mode}")
 
     def __call__(self, trial_id, result) -> bool:
         epoch = result["epoch"]
         threshold = self.get_threshold(epoch)
-        if self.mode == "max":
-            return result[self.metric] > threshold
-        elif self.mode == "min":
-            return result[self.metric] < threshold
-        else:
-            raise ValueError(f"Invalid mode {self.mode}")
+        if isnan(threshold):
+            return False
+        return self._better_than(result[self.metric], threshold)
 
     def stop_all(self) -> bool:
         return False
@@ -55,28 +64,34 @@ class NoImprovementStopper(tune.Stopper):
         self.mode = mode
         self.patience = patience
         self.grace_period = grace_period
-        self._best: DefaultDict[Any, None | int] = collections.defaultdict(lambda: None)
+        self._best: DefaultDict[Any, None | float] = collections.defaultdict(
+            lambda: None
+        )
         self._stagnant: DefaultDict[Any, int] = collections.defaultdict(int)
         self._epoch: DefaultDict[Any, int] = collections.defaultdict(int)
+
+    def _better_than(self, a: float, b: float) -> bool:
+        """Is result a better than result b?"""
+        try:
+            ratio = a / b
+        except ZeroDivisionError:
+            ratio = None
+        if ratio is None:
+            return False
+        if self.mode == "max" and ratio > 1 + self.rel_change_thld:
+            return True
+        if self.mode == "min" and ratio < 1 + self.rel_change_thld:
+            return True
+        return False
 
     def __call__(self, trial_id, result) -> bool:
         self._epoch[trial_id] += 1
         if self._best[trial_id] is None:
             self._best[trial_id] = result[self.metric]
             return False
-        try:
-            ratio = result[self.metric] / self._best[trial_id]
-        except ZeroDivisionError:
-            ratio = None
-        if (
-            self.mode == "max"
-            and ratio is not None
-            and ratio > 1 + self.rel_change_thld
-        ) or (
-            self.mode == "min"
-            and ratio is not None
-            and ratio < 1 - self.rel_change_thld
-        ):
+        best_result = self._best[trial_id]
+        assert best_result is not None  # for mypy
+        if self._better_than(result[self.metric], best_result):
             self._best[trial_id] = result[self.metric]
             self._stagnant[trial_id] = 0
             return False
