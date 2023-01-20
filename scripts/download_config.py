ce969c55f3d031ad089fbce5ae75bae83452a3d9
Kilian Lieret <kilian.lieret@posteo.de>
2023-01-20 14:52:30 -0500

    Support multiple configs to download

--- scripts/download_config.py
+++ scripts/download_config.py
@@ -7,10 +7,14 @@ from argparse import ArgumentParser
 from pathlib import Path
 
 from gnn_tracking_hpo.config import retrieve_config_from_wandb
+from gnn_tracking_hpo.util.log import logger
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("hash")
+    parser.add_argument("hash", nargs="+", dest="hashes")
     args = parser.parse_args()
-    config = retrieve_config_from_wandb(args.hash)
-    Path(f"{args.hash}.json").write_text(json.dumps(config, indent=4))
+    for this_hash in args.hashes:
+        config = retrieve_config_from_wandb(this_hash)
+        output_path = f"{this_hash}.json"
+        Path(output_path).write_text(json.dumps(config, indent=4))
+        logger.info("Wrote to %s", output_path)
