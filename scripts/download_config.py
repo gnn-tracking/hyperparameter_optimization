949d95aedefd42cf1f8b2b6ec3f93de3e5b2c1c2
Kilian Lieret <kilian.lieret@posteo.de>
2023-01-21 18:08:14 -0500

    Add output option; be fault tolerant

--- scripts/download_config.py
+++ scripts/download_config.py
@@ -12,9 +12,17 @@ from gnn_tracking_hpo.util.log import logger
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("hashes", nargs="+")
+    parser.add_argument(
+        "--output", "-o", help="Output directory", default=".", type=Path
+    )
     args = parser.parse_args()
+    args.output.mkdir(exist_ok=True, parents=True)
     for this_hash in args.hashes:
-        config = retrieve_config_from_wandb(this_hash)
-        output_path = f"{this_hash}.json"
+        try:
+            config = retrieve_config_from_wandb(this_hash)
+        except ValueError:
+            logger.error(f"Could not retrieve config for hash {this_hash}")
+            continue
+        output_path = args.output / f"{this_hash}.json"
         Path(output_path).write_text(json.dumps(config, indent=4))
         logger.info("Wrote to %s", output_path)
