79daf16c73acdace3e74a1a6c714f9e57b638f5c
Kilian Lieret <kilian.lieret@posteo.de>
2023-01-24 12:20:50 -0500

    Add shellcheck pre-commit config

--- slurm/memory_test.py
+++ slurm/memory_test.py
@@ -1,4 +1,5 @@
-from gnn_tracking.graph_construction.graph_builder import load_graphs
+from __future__ import annotations
 
+from gnn_tracking.graph_construction.graph_builder import load_graphs
 
 graphs = load_graphs("/tigress/jdezoort/object_condensation/graphs")
