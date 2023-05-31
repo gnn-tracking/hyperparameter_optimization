fdf6fb6cb74b6355b9e112279f6f11ac2cad108d
Kilian Lieret <kilian.lieret@posteo.de>
2023-05-30 20:49:22 -0400

    Add NoNaNStopper

--- scripts/tune_gc.py
+++ scripts/tune_gc.py
@@ -1,11 +1,14 @@
 from __future__ import annotations
 
+import collections
 from argparse import ArgumentParser
-from typing import Any
+from math import isnan
+from typing import Any, DefaultDict
 
 import optuna
 from gnn_tracking.training.lw_setter import LinearLWSH
 from gnn_tracking.training.tcn_trainer import TCNTrainer
+from ray import tune
 from rt_stoppers_contrib import NoImprovementTrialStopper
 
 from gnn_tracking_hpo.config import auto_suggest_if_not_fixed, get_metadata
@@ -25,7 +28,7 @@ class LWSGCTrainable(GCTrainable):
             loss_name=("potential", "repulsive"),
             start_value=self.tc["lws_repulsive_llw_start_value"],
             end_value=self.tc["lw_potential_repulsive"],
-            end=self.tc["lws_potential_llw_end"],
+            end=self.tc["lws_repulsive_llw_end"],
             n_batches=800,
         )
 
@@ -79,31 +82,56 @@ def suggest_config(
     # Tuned parameters
     # ----------------
 
-    d("lw_potential_repulsive", [1e-2, 5e-2, 1e-1])
+    final_potential = d("lw_potential_repulsive", 1e-1, 1)
 
     # d("adam_weight_decay", 0)
     # d("adam_beta1", 0.9, 0.99)
     # d("adam_beta2", 0.9, 0.9999)
 
-    d("lws_repulsive_llw_start_value", [5e-4, 1e-3, 5e-3])
-    d("lws_potential_llw_end", [1, 3, 5, 9])
+    d("lws_repulsive_llw_start_value", 5e-4)
+    if final_potential > 0.5:
+        d("lws_repulsive_llw_end", 9, 20)
+    else:
+        d("lws_repulsive_llw_end", 1, 9)
 
     suggest_default_values(config, trial, hc="none", ec="none")
     return config
+
+
+class NoNaNStopper(tune.Stopper):
+    def __init__(self, settings: dict[str, tuple[int, int]]):
+        self._settings = settings
+        self._epoch: DefaultDict[Any, int] = collections.defaultdict(int)
+        self._n_consecutive_nans: Any = collections.defaultdict(
+            lambda: collections.defaultdict(int)
+        )
+
+    def __call__(self, trial_id: str, result: dict) -> bool:
+        self._epoch[trial_id] += 1
+        for metric, (min_epoch, patience) in self._settings.items():
+            if self._epoch[trial_id] < min_epoch:
+                continue
+            if isnan(result[metric]):
+                self._n_consecutive_nans[trial_id][metric] += 1
+                if self._n_consecutive_nans[trial_id][metric] >= patience:
+                    return True
+            else:
+                self._n_consecutive_nans[trial_id][metric] = 0
+        return False
+
+    def stop_all(self) -> bool:
+        return False
 
 
 class MyDispatcher(Dispatcher):
     def get_no_improvement_stopper(self) -> NoImprovementTrialStopper:
         return NoImprovementTrialStopper(
             metric="total",
-            patience=3,
+            patience=5,
             mode="min",
-            grace_period=3,
+            grace_period=5,
             rel_change_thld=0.01,
         )
-
-    # def get_optuna_sampler(self):
-    #     return optuna.samplers.RandomSampler()
 
 
 if __name__ == "__main__":
@@ -111,7 +139,20 @@ if __name__ == "__main__":
     add_common_options(parser)
     kwargs = vars(parser.parse_args())
     assert kwargs["wandb_project"] == "gnn_tracking_gc"
+
+    nn_stopper = NoNaNStopper(
+        {
+            "n_edges_frac_segment50_80": (5, 3),
+            "n_edges_frac_segment50_90": (8, 3),
+            "n_edges_frac_segment50_93": (12, 4),
+        }
+    )
     dispatcher = MyDispatcher(
-        **kwargs, metric="n_edges_frac_segment50_90", grace_period=4, comparison="min"
+        **kwargs,
+        metric="n_edges_frac_segment50_80",
+        grace_period=10,
+        comparison="min",
+        additional_stoppers=[nn_stopper],
+        no_scheduler=True,
     )
     dispatcher(LWSGCTrainable, suggest_config)
